efa2df0ee73dfcbef9d9c6df04746e21421475db
moose <mr.rammo@gmail.com>
2018-01-15 12:11:07 -0500

    link resume to google drive

--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,5 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index');
 });
-router.get('/resume', function(req, res, next) {
-  res.render('resume')
-});
 
 module.exports = router;
